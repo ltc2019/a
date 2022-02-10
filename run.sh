655d98eb98e395d7801360bb56a18d7a9e0bb2db
ltc2019 <15108968441@163.com>
2022-02-10 18:02:05 +0800

    update

--- run.sh
+++ run.sh
@@ -6,3 +6,7 @@ git remote rm origin
 git pull --a origin main
 git remote add origin git@github.com:ltc2019/a.git
 git push -u origin main
+
+touch /d/运行日志.txt
+sleep 10
+echo "本地更新文件已经自动上传到Github">> /d/运行日志.txt
